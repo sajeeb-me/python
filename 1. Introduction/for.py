dbd768bffbcb04ecbc0376c377d05efe673cadfc
sdshuvo98 <hellosdshuvo@gmail.com>
2023-07-02 23:11:26 +0100

    for loop

---
+++ 1. Introduction/for.py
@@ -0,0 +1,16 @@
+numbers = [5, 10, 15, 20, 25]
+
+sum = 0
+for num in numbers:
+    sum += num
+    # print(num)
+    print(sum)
+print("total: ",sum)
+
+
+text = "e hawa"
+for char in text:
+    print(char)
+
+for i in range(1, 10, 2):
+    print(i);
