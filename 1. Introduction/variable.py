40cc210d66133c8ad6c4c62d9643be226d9eacd4
sdshuvo98 <hellosdshuvo@gmail.com>
2023-07-02 13:20:30 +0100

    variable

---
+++ 1. Introduction/variable.py
@@ -0,0 +1,15 @@
+age = 30
+interest_rate = 2.5
+name = "My name is khan"
+district = 'B baria'
+is_single = True
+
+print(age + interest_rate)
+print(district)
+
+print(type(age))
+print(type(is_single))
+
+print("I am the" + " " + "best")
+text = f"{name}. I am living in {district}"
+print(text)
