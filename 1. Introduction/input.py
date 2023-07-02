4eff58538d9f7e5e3ca9a1f80a83ab09c2d9fdc8
sdshuvo98 <hellosdshuvo@gmail.com>
2023-07-02 13:30:59 +0100

    input in python

---
+++ 1. Introduction/input.py
@@ -0,0 +1,13 @@
+# print("Now I need money")
+# input()
+
+# input("Give me some money: ")
+
+# money = input("Give me some money: ")
+# print("here is your money: ",money)
+
+first_money = input("Jack, Give me some money: ")
+second_money = input("Danials, give me some money: ")
+first_money_int = int(first_money)
+second_money_int = int(second_money)
+print("My total money: ",first_money_int + second_money_int)
