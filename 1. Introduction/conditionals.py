7fa68ac425d08fcbd37f8323e9abf8b248cf6658
sdshuvo98 <hellosdshuvo@gmail.com>
2023-07-02 20:07:34 +0100

    conditionals

---
+++ 1. Introduction/conditionals.py
@@ -0,0 +1,12 @@
+# in, not, not in, is, is not
+
+a = 7
+if a>5:
+    print("5 er besi")
+    print("Koto besi k jane")
+elif a>3:
+    print("Olpo boro")
+elif a==2:
+    print("2 er soman soman")
+else:
+    print("5 er kom")
