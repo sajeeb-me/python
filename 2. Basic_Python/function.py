1d69de569dfc73d0dca5d4856173ae094901999e
sdshuvo98 <hellosdshuvo@gmail.com>
2023-07-03 21:23:43 +0100

    function

---
+++ 2. Basic_Python/function.py
@@ -0,0 +1,13 @@
+def double_it(num):
+    result = num*2
+    print(result)
+
+double_it(5)
+
+def sum(num1, num2):
+    return num1+num2
+
+sum1 = sum(70, 45)
+sum2 = sum(20, 15)
+print("Sum 1: ", sum1)
+print("Sum 2: ",sum2)
